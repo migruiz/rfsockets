30211e97be041fbca40fdeaf20fcd06b936a58c2
Miguel <mig.ruiz@gmail.com>
2019-12-30 05:25:37 +0000

    fixing travis

--- travis.sh
+++ travis.sh
@@ -1,16 +1,7 @@
 #!/bin/bash  
 set -ev
-HUBNAME=""
-if [[ -z "$TRAVIS_TAG" ]]; then
-	if [ "$TRAVIS_BRANCH" = "master" ]; then
-		HUBNAME=migruiz/$PI_APP-$PI_TYPE;
-	else
-		HUBNAME=migruiz/$PI_APP-$PI_TYPE:$TRAVIS_BRANCH
-	fi   
-else
-	HUBNAME=migruiz/$PI_APP-$PI_TYPE:$TRAVIS_TAG;
-fi
+HUBNAME=migruiz/$PI_APP
 docker pull $HUBNAME || true
-docker build --cache-from $HUBNAME  -t $HUBNAME  . 
+docker build -f Dockerfile --cache-from $HUBNAME  -t $HUBNAME  . 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME  
